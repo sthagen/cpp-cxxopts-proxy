badc594bfb7435e1da14802fbec0fb0f155418d8
Stefan Hagen <stefan@hagen.link>
2021-09-13 13:26:23 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -27,7 +27,7 @@ TEST_SUITE ("Example derived tests.") {
             ("d,debug", "Enable debugging")
             ("f,file", "File name", cxxopts::value<std::string>());
 
-        char *argv[] = {"-h", NULL};
+        char *argv[] = {(char *)"-h", NULL};
         int argc = sizeof(argv) / sizeof(char*) - 1;
         auto result = options.parse(argc, argc);
             REQUIRE(result);
